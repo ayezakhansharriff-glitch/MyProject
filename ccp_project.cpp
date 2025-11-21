63232cfec4f9188f2e032a2c6e1ac828acbfb60b
Ayeza sharif <ayezakhan.sharriff@gmail.com>
2025-11-21 21:49:13 +0500

    Add files via upload

---
+++ ccp_project.cpp
@@ -0,0 +1,29 @@
+Project Description
+
+Title: Oracle’s Caves – Text-Based Adventure Game in C
+
+This project is a console-based adventure game written in C. The player wakes up inside ancient caves once ruled by an old civilization called the Arkenites. Their goal is to collect three sacred relics — the Ancient Key, the Oracle’s Tome, and the Seal of Wisdom — in order to unlock the final chamber and meet The Oracle.
+
+The game includes:
+
+Story introduction
+
+Multiple rooms (Left Chamber, Old Library, Puzzle Room, Final Door)
+
+Combat system with enemies
+
+Riddle challenges
+
+Health, sanity, and item management
+
+Rest system to recover HP and sanity
+
+Inventory system using dynamic memory (malloc)
+
+Player stats like strength, intelligence, agility
+
+Structs for player and enemy
+
+Random events using rand()
+
+The player must explore the chambers, solve puzzles, survive fights, and gather all three relics to access the Oracle’s Chamber. The game ends with a story-based conclusion when the player succeeds.
